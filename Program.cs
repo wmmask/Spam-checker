a67a556032b1868db1bef7459221db653238291f
wmmask <wf1029@post.com>
2020-07-24 18:28:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Spam_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string blackListWord "psycho";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            //message/string for test with spam:
+                //The company has a pyscho culture.
+            //message/string for test without spam:
+            //The company is forward thinking!
+
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam.");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+            }
+
+        }
+    }
+}
